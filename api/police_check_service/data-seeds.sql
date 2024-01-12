4a016c7edbbaa089abd897ce34294f2834a5906c
shamly-shanawaz <m.shamlyshanawaz@gmail.com>
2024-01-12 13:16:43 +0530

    changes

---
+++ api/police_check_service/data-seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Citizen(nic) VALUES ('20040');
+INSERT INTO Citizen(nic) VALUES ('20041');
+
+INSERT INTO PoliceCase(issue, date, citizenNic) VALUES ('road rage', '2023-11-12', '20040');
+INSERT INTO PoliceCase(issue, date, citizenNic) VALUES ('attempted murder', '2023-01-02', '20041');
