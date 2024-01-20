e550101f60c0fb8872a03c84a2d0228deb81e1a4
Haritha Hasathcharu <haritha@wso2.com>
2024-01-20 12:07:54 +0530

    asgardeo integration wip

--- mobileapp/grama-check/context/AuthContext.js
+++ mobileapp/grama-check/context/AuthContext.js
@@ -5,11 +5,9 @@ import * as AuthSession from 'expo-auth-session';
 import 'core-js/stable/atob';
 
 export const AuthContext = createContext(null);
-export const TOKEN_ENDPOINT =
-  'https://api.asgardeo.io/t/hasathcharu/oauth2/token';
+export const TOKEN_ENDPOINT = 'https://api.asgardeo.io/t/interns/oauth2/token';
 export const redirectUri = AuthSession.makeRedirectUri();
-export const CLIENT_ID = 'QMK8Jwlm0e5WTj7Ij0Jk1lBNSh4a';
-
+export const CLIENT_ID = 'OoXOfHIW8C8fcft8ouqJtVCgaVwa';
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const checkAuth = async function () {
