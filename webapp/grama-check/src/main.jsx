1366526bffd62e1874fa9ad854bf3691d84d151b
Seefa Banu <114132712+seefaBanu@users.noreply.github.com>
2024-02-26 16:57:15 +0530

    minor chnnges

--- webapp/grama-check/src/main.jsx
+++ webapp/grama-check/src/main.jsx
@@ -8,9 +8,9 @@ const config = {
   // signInRedirectURL: "localhost:5173",
   signInRedirectURL: "https://c6313fcb-3c26-424b-b361-1141edab3fb8.e1-us-east-azure.choreoapps.dev",
   // signOutRedirectURL: "http://localhost:5173",
-  clientID: "OoXOfHIW8C8fcft8ouqJtVCgaVwa",
+  clientID: "4pvNZCwIhW_ZDY9b3PlkWWcy3vsa",
   baseUrl: "https://api.asgardeo.io/t/interns",
-  scope : ["openid", "profile", "email"],
+  scope : ["openid", "profile", "email", "urn:interns:gcgeneralservicegeneral:grama"],
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
