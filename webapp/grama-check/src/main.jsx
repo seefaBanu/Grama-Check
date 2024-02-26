2f824951e5d079f0fcc11a86b096f969a42092f8
Seefa Banu <114132712+seefaBanu@users.noreply.github.com>
2024-02-26 16:40:31 +0530

    minor changes

--- webapp/grama-check/src/main.jsx
+++ webapp/grama-check/src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import { AuthProvider } from '@asgardeo/auth-react'
 
 const config = {
-  signInRedirectURL: "localhost:5173",
+  // signInRedirectURL: "localhost:5173",
+  signInRedirectURL: "https://c6313fcb-3c26-424b-b361-1141edab3fb8.e1-us-east-azure.choreoapps.dev",
   // signOutRedirectURL: "http://localhost:5173",
   clientID: "OoXOfHIW8C8fcft8ouqJtVCgaVwa",
   baseUrl: "https://api.asgardeo.io/t/interns",
